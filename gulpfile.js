c7b67004cf99be6bc6c04a4788ab58dbf655a9e7
Milton Camara <miltomcamara@gmail.com>
2017-05-12 02:33:56 -0300

    Movile Version

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,67 @@
-var gulp 				= require("gulp");
-var sass 				= require("gulp-sass");
-var htmlmin 		= require("gulp-htmlmin");
-var notify 			= require("gulp-notify");
-var concat 			= require("gulp-concat");
-var uglify 			= require("gulp-uglify");
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var htmlmin = require("gulp-htmlmin");
+var notify = require("gulp-notify");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 var browserSync = require("browser-sync").create();
-var del 				= require("del");
+var del = require("del");
 
 /* Tasks cached */
-gulp.task("cache:css", function() {
+gulp.task("cache:css", function () {
 	del("./dist/css/style.css")
 });
 
-gulp.task("cache:js", function() {
+gulp.task("cache:js", function () {
 	del("./dist/js/app.js")
 });
 
 /* Task compile scss to css */
-gulp.task("sass", ['cache:css'], function() {
+gulp.task("sass", ['cache:css'], function () {
 	return gulp.src("./src/scss/style.scss")
-				.pipe(sass({outPutStyle: 'compressed'}))
-				.on('error', notify.onError({title: "erro scss", message: "<%= error.message %>"}))
-				.pipe(gulp.dest("./dist/css"))
-				.pipe(browserSync.stream());
+		.pipe(sass({
+			outPutStyle: 'compressed'
+		}))
+		.on('error', notify.onError({
+			title: "erro scss",
+			message: "<%= error.message %>"
+		}))
+		.pipe(gulp.dest("./dist/css"))
+		.pipe(browserSync.stream());
 });
 
 /* Task minify html */
-gulp.task("html", function() {
+gulp.task("html", function () {
 	return gulp.src("./src/index.html")
-				.pipe(htmlmin({collapseWhitespace: true}))
-				.pipe(gulp.dest("./dist"))
-				.pipe(browserSync.stream());
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest("./dist"))
+		.pipe(browserSync.stream());
 });
 
 /* Task minify js */
-gulp.task("js", ['cache:js'], function() {
+gulp.task("js", ['cache:js'], function () {
 	return gulp.src("./src/js/app.js")
-				.pipe(uglify())
-				.pipe(gulp.dest("./dist/js"))
-				.pipe(browserSync.stream());
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist/js"))
+		.pipe(browserSync.stream());
 });
 
 /* Task concat js */
-gulp.task("concat-js", function() {
+gulp.task("concat-js", function () {
 	return gulp.src([
-					'./src/components/jquery/dist/jquery.js',
-					'./src/components/tether/dist/js/tether.js',
-					'./src/components/bootstrap/dist/js/bootstrap.js'
-				])
-				.pipe(concat("main.js"))
-				.pipe(gulp.dest("./dist/js"))
+			'./src/components/jquery/dist/jquery.js',
+			'./src/components/tether/dist/js/tether.js',
+			'./src/components/bootstrap/dist/js/bootstrap.js'
+		])
+		.pipe(concat("main.js"))
+		.pipe(gulp.dest("./dist/js"))
 
 });
 
 /* Task server local */
-gulp.task("server", function() {
+gulp.task("server", ["sass", "html", "js", "concat-js"], function () {
 	browserSync.init({
 		server: {
 			baseDir: "./dist"
@@ -67,15 +74,3 @@ gulp.task("server", function() {
 	gulp.watch("./src/js/**/*.js", ['js']);
 	gulp.watch("./src/index.html", ['html']);
 });
-
-
-
-
-
-
-
-
-
-
-
-
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-$(function() {
-    $("html, body").click(function(e) {
-        if(e.target == document.documentElement){
+$(function () {
+    $("a").click(function (e) {
+        e.preventDefault();
+    });
+
+    $("html, body").click(function (e) {
+        if (e.target == document.documentElement) {
             $(this).removeClass("opened-side-menu");
         }
+    });
+
+    $(".toogle-sidebar a").click(function (e) {
+        e.preventDefault();
+        $("html").addClass("opened-side-menu");
     });
 });
